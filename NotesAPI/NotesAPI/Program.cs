18d453a7988ef339b43e608007ccd92433fb6dcc
Kevin Grimaldi <anthgrim@icloud.com>
2022-11-18 16:58:05 -0500

    Removing allowanyorigin from cors and adding specific origins to program.cs

--- NotesAPI/NotesAPI/Program.cs
+++ NotesAPI/NotesAPI/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseCors(policy => policy.WithOrigins("http://localhost:3000", "localhost:3000").AllowAnyHeader().AllowAnyMethod());
 
 app.UseAuthorization();
 
